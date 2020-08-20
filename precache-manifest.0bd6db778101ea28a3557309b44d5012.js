121a2b94e2ccaee2b44a35112920980504ab6d5d
mgm <tommin231@gmail.com>
2020-08-21 00:07:58 +0900

    Updates

---
+++ precache-manifest.0bd6db778101ea28a3557309b44d5012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737c5f169eb59b3bad5cb7db9ed0e7a2",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f160fbc3bbba8e07990f",
+    "url": "/movie_app_2020/static/css/main.39bf97f3.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f160fbc3bbba8e07990f",
+    "url": "/movie_app_2020/static/js/main.f619b0a3.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
